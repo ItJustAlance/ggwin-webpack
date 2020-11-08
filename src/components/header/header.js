da7477c446b6393415c1da21a6aeff7a10b940b2
r399055750 <r399055750@gmail.com>
2020-11-08 16:37:58 +0500

    mobile version

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -14,8 +14,23 @@
       document.querySelector('.js-open-market').onclick = function() {
         document.body.classList.toggle('market-show');
       }
+      document.querySelector('.inventar-market--close').onclick = function() {
+        document.body.classList.toggle('market-show');
+      }
       document.querySelector('.chat-free__arrow').onclick = function() {
         document.body.classList.toggle('free-skin__open');
+      }
+      document.querySelector('.float-btn-chat').onclick = function() {
+        document.body.classList.add('float-chat__open');
+      }
+      document.querySelector('.chat-close-close').onclick = function() {
+        document.body.classList.remove('float-chat__open');
+      }
+      document.querySelector('#hamburger-icon').onclick = function() {
+        document.body.classList.add('active_m');
+      }
+      document.querySelector('.header-toggle__close').onclick = function() {
+        document.body.classList.remove('active_m');
       }
 
 
