000c1a0c6a5b38c82ac66dd8c99ce9e9487f2d2e
r399055750 <r399055750@gmail.com>
2020-11-14 23:39:50 +0500

    исправление багов

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -9,6 +9,7 @@
         $('body').addClass('chat-hide');
         $('body').removeClass('float-chat__open');
       });
+
       $(".chat-open").on('click', function() {
         $('body').removeClass('chat-hide');
       });
@@ -34,9 +35,43 @@
       $("#hamburger-icon").on('click', function() {
         $('body').addClass('active_m');
       });
-      $(".header-toggle__close").on('click', function() {
-        $('body').removeClass('active_m');
-      });
+        $(".header-toggle__close").on('click', function() {
+            $('body').removeClass('active_m');
+        });
+
+// //////////// popups
+        $(".popup__close").on('click', function() {
+            $('body').removeClass();
+        });
+        $(".js-pop-popolnit").on('click', function() {
+            $('body').addClass("popup-show-popolnit");
+        });
+        $(".js-pop-edit-user").on('click', function() {
+            $('body').addClass("pop-edit-user__show");
+        });
+        $(".js-pop-edit-ava").on('click', function() {
+            $('body').addClass("pop-edit-ava__show");
+        });
+        $(".js-pop-create-ticket").on('click', function() {
+            $('body').addClass("pop-create-ticket__show");
+        });
+        $(".js-pop-inline").on('click', function() {
+            $('body').addClass("pop-inline__show");
+        });
+
+
+        $(".j-tab-user-ava").on('click', function() {
+            $('.popup-tabs__item').removeClass('active');
+            $(this).addClass('active');
+            $('#tab__user-ava').show();
+            $('#tab__user-nick').hide();
+        });
+        $(".j-tab-user-nick").on('click', function() {
+            $('.popup-tabs__item').removeClass('active');
+            $(this).addClass('active');
+            $('#tab__user-nick').show();
+            $('#tab__user-ava').hide();
+        });
 
 
     }, // end init
